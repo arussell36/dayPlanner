c77b25ee062336a6d8e913ec40a8042aa05e17e7
arussell36 <andrewrussell036@gmail.com>
2019-10-08 18:13:22 -0500

    pseudocode for assignment

---
+++ script.js
@@ -0,0 +1,23 @@
+// in the header there is a dynamic field that grabs the date
+
+
+// VARIABLES
+    // event to grab todays date
+
+
+// HELPER FUNCTIONS
+
+
+// EVENTS
+    // 1. clicking save button on the end of the string in each row 
+    // - this should store text in local storage for whatever is in the input box
+
+// INIT
+    // 1. check local storage object for matching ID
+    // 2. color code past present and future rows
+
+
+
+
+
+
